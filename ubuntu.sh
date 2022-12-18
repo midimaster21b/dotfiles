ec73e57292ec19d2b9660e2e8bcf06ad6f48dcf7
Joshua Edgcombe <joshedgcombe@gmail.com>
2022-12-18 14:37:03 -0500

    Add Vivado prerequisite libraries

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,7 +5,10 @@ cp .quotes ~
 cp .tmux.conf ~
 cp -r .emacs.d ~
 
-# Install the libraries
+# Install Xilinx Vivado prerequisite libraries
+sudo apt -y install libncurses5 libtinfo5 libncurses5-dev libncursesw5-dev
+
+# Install python prerequisite libraries
 sudo apt -y install libsqlite3-dev libreadline-dev zlib1g-dev libssl-dev libbz2-dev libffi-dev liblzma-dev
 
 # Install the tools
